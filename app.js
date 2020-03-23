6e31978a4e137723023e174265f4cd58bec37eb3
matt <you@example.com>
2020-03-22 17:59:53 -0700

    added a new if statement

--- app.js
+++ app.js
@@ -8,18 +8,29 @@ var action = process.argv[2];
 // var command = process.argv[3];
 
 var command = process.argv.splice(3).join(" ")
-
+// spotify this song Bruno Mars
 
 // console.log(action);
 // console.log(command);
 
 
 // function dogtowel(action, command){
-     if (action) { 
-        console.log(chalk.yellow(action))
-    }
-    if (command) { 
-        console.log(chalk.red(command));
-    };
+    //  if (action) { 
+    //     console.log(chalk.yellow(action))
+    // }
+    // if (command) { 
+    //     console.log(chalk.red(command));
+    // };
 // };
 
+
+if (action === "spotify"){
+    console.log("spotify")
+    console.log(action, "\n", command)
+} else if (action === "music"){
+    console.log("music")
+    console.log("\n", command)
+} else if (action === "tidal") {
+    console.log("tidal")
+    console.log(action, "\n", command)
+}
