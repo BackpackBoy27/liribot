85f82c17efd08773969c4a9a081ce18df4a6fe3c
matt <you@example.com>
2020-03-22 19:09:33 -0700

    final layout

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+console.log(process.env.OMDB_KEY)
 var chalk = require("chalk");
-// var axios = require("axios");
-// var spotify = require("node-spotify-api");
+var axios = require("axios");
+var Spotify = require("node-spotify-api");
 // var moment = require("moment");
 // var dotenv = require("dotenv");
 
@@ -23,14 +25,30 @@ var command = process.argv.splice(3).join(" ")
     // };
 // };
 
+let spotify = new Spotify({
+    id:process.env.SPOTIFY_ID, 
+    secret:process.env.SPOTIFY_SECRET
+})
 
 if (action === "spotify"){
     console.log("spotify")
     console.log(action, "\n", command)
+
+spotify.search({
+    type:"track",
+    query:action,
+    limit: 1
+},(error, data) => {
+console.log(data.tracks)
+})
+
 } else if (action === "music"){
     console.log("music")
     console.log("\n", command)
+
 } else if (action === "tidal") {
     console.log("tidal")
     console.log(action, "\n", command)
+    
 }
+
